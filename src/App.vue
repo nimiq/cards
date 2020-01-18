b1e21e515380c3fe765ed7ae251c1b214b785ddd
Daniel <github.danimoh@neverbox.com>
2020-01-18 15:02:27 -0600

    Remove unnecessary files

--- src/App.vue
+++ src/App.vue
@@ -203,7 +203,6 @@ export default class App extends Vue {
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: left;
-        // color: #2c3e50;
         margin-top: 60px;
         display: flex;
         justify-content: center;
