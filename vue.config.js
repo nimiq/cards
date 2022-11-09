dfa60a63d78d1de0773250e8ce5e281f8305d0b6
Sören <hello@soerenschwert.de>
2022-11-09 15:20:14 +0100

    Fix build for NodeJS version with OpenSSL3
    Node 16 on Fedora and Node 17+ on other systems.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,11 @@
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
+// Fix build for Node version with OpenSSL 3
+const crypto = require('crypto');
+
+const origCreateHash = crypto.createHash;
+crypto.createHash = (alg, opts) => origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
+
 module.exports = {
     configureWebpack: {
         plugins: [
