427c4a5500116444f766dd929e31e8f52b4f05eb
Daniel <github.danimoh@neverbox.com>
2022-11-22 23:38:27 -0600

    Update vue-components for fix for M1 macOS Ventura BarcodeDetector in qr-scanner
    Also adapt the build process accordingly, because the qr-scanner worker is now bundled
    automatically and does not have to be manually copied over.

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
-const CopyWebpackPlugin = require('copy-webpack-plugin');
-
 // Fix build for Node version with OpenSSL 3
 const crypto = require('crypto');
 
@@ -7,13 +5,6 @@ const origCreateHash = crypto.createHash;
 crypto.createHash = (alg, opts) => origCreateHash(alg === 'md4' ? 'md5' : alg, opts);
 
 module.exports = {
-    configureWebpack: {
-        plugins: [
-            new CopyWebpackPlugin([
-                { from: 'node_modules/qr-scanner/qr-scanner-worker.*', to: 'js', flatten: true },
-            ]),
-        ],
-    },
     outputDir: 'deployment/dist',
     publicPath: '',
 };
